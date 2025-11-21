a9e8fc975661f20bd5d9b24fcbeda5174d66ba61
Pruthviraj Chavan <pruthvirajchavan6877@email.com>
2025-11-21 12:27:41 +0530

    question 2

--- Python/Day16 Tuple (Practice).py
+++ Python/Day16 Tuple (Practice).py
@@ -1,8 +1,19 @@
 
 """Tuple Practice Set"""
 
-#.....1) Create a tuple with five numbers and print it.
+""".....1) Create a tuple with five numbers and print it."""
+
+# t = (10,20,30,40,50)
+# print(t)
+
+
+""".....2) Access the first and last element of a tuple."""
 
 t = (10,20,30,40,50)
-print(t)
+first_element = t[0]
+last_element = t[-1]
+print(f"first element : {first_element} and last element : {last_element}")
+
+
+
 
