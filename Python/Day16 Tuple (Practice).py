20417971dcfd3e00bf3eacddae7f47217f3e7d5c
Pruthviraj Chavan <pruthvirajchavan6877@email.com>
2025-11-21 12:36:18 +0530

    question 3

--- Python/Day16 Tuple (Practice).py
+++ Python/Day16 Tuple (Practice).py
@@ -9,11 +9,23 @@
 
 """.....2) Access the first and last element of a tuple."""
 
-t = (10,20,30,40,50)
-first_element = t[0]
-last_element = t[-1]
-print(f"first element : {first_element} and last element : {last_element}")
+# t = (10,20,30,40,50)
+# first_element = t[0]
+# last_element = t[-1]
+# print(f"first element : {first_element} and last element : {last_element}")
 
+
+""".....3) Check whether an element exists in a tuple."""
+
+t = (10,23,45,3,5,35,34,78)
+a = 34
+
+for i in range(len(t)): 
+    if t[i] == a: 
+        print("this element exists in the tuple")
+        break
+else: 
+    print("this element does not exist in the tuple")
 
 
 
