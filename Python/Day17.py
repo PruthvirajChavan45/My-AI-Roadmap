fdfb36adff070f282f0f6acf4c64e7506ea0aa93
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-22 16:41:36 +0530

    What is Set?

---
+++ Python/Day17.py
@@ -0,0 +1,9 @@
+
+"""....3) Set in Python...."""
+
+#  - A set is a built-in data structure in Python used to store 
+#    multiple items in a single variable, but:
+
+#       - Order is NOT guaranteed.
+#       - Duplicate values are NOT allowed.
+
