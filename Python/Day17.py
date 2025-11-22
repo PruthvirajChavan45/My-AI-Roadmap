0c3c11294833fda767ef2722b73d198ea41294a0
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-22 16:43:29 +0530

    Properties of a set

--- Python/Day17.py
+++ Python/Day17.py
@@ -7,3 +7,12 @@
 #       - Order is NOT guaranteed.
 #       - Duplicate values are NOT allowed.
 
+
+"""....Properties of a Set...."""
+
+# 1) Unordered  -  No fixed index / arrangement
+# 2) Unindexed	- Cannot access values by index
+# 3) No duplicates - Automatically removes repeated values
+# 4) Mutable - You can add or remove elements
+# 5) Mixed data allowed	- Integers, strings, floats, etc.
+
