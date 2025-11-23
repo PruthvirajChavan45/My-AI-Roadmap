c86265ef6e451f0da8c5252b214ab5f198b4fc40
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-23 10:33:23 +0530

    Example 9 and 10

--- Python/Day18 Set (Practice).py
+++ Python/Day18 Set (Practice).py
@@ -27,7 +27,6 @@
 # print(s)
 
 
-
 """.....5) Check whether a given element exists in a set or not."""
 
 # s = {10,20,30,40,50,60,70,80,90,100}
@@ -54,7 +53,27 @@
 
 """.....8) Find the union of two sets."""
 
-a = {1,2,3,4}
-b = {3,4,5,6}
-print(a.union(b))
+# a = {1,2,3,4}
+# b = {3,4,5,6}
+# print(a.union(b))
+
+
+""".....9) Find the intersection of two sets."""
+
+# a = {1,2,3,4}
+# b = {3,4,5,6}
+
+# print(a.intersection(b))
+
+
+""".....10) Find the difference of two sets (A - B)."""
+
+A = {1,2,3,4}
+B = {3,4,5,6}
+
+print(A - B)
+
+
+
+
 
