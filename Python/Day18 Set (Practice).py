e80c68ce488f7da926aacf41f33184c9ae73c232
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-23 10:00:17 +0530

    Example 3

--- Python/Day18 Set (Practice).py
+++ Python/Day18 Set (Practice).py
@@ -8,7 +8,13 @@
 
 """.....2) Add 2 new elements to an existing set."""
 
-s = {10,20,30,40,50,60,70,80,90,100}
-s.update([25,35])
-print(s)
+# s = {10,20,30,40,50,60,70,80,90,100}
+# s.update([25,35])
+# print(s)
 
+
+""".....3) Remove an element from a set using remove()."""
+
+s = {10,20,30,40,50,60,70,80,90,100}
+s.remove(40)
+print(s)
