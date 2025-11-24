1c492fd84c76df0c9dc00b68bf2b94a601949ba1
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-24 16:37:18 +0530

    Dictionary in python

---
+++ Python/Day19.py
@@ -0,0 +1,16 @@
+
+"""....4) Dictionary in Python...."""
+#   A dictionary is a collection of key–value pairs.
+
+#   Syntax :
+#       dict_name = {key: value, key: value, ...}
+
+#   Example : 
+
+student = {
+    "name": "Pruthviraj",
+    "age": 22,
+    "course": "Python"
+}
+
+
