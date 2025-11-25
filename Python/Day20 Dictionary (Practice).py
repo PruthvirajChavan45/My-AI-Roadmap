eed05b18851ecb9ebcab5e175a4e628d9afcf329
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-25 09:56:59 +0530

    Example 3

--- Python/Day20 Dictionary (Practice).py
+++ Python/Day20 Dictionary (Practice).py
@@ -26,14 +26,38 @@
     a dictionary or hash map.
 """
 
-a = {1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,6,6,6,7}
+# a = {1,1,2,2,2,3,3,3,3,4,4,4,4,4,5,5,5,6,6,6,7}
 
+# d = {}
+
+# for i in a: 
+#     if i in d.keys(): 
+#         d[i] += 1
+#     else: 
+#         d[i] = 1
+# print(d)
+
+
+#.....3) Leetcode 771 - jewels and stones.....
+"""
+    count how many stones are also jewels
+    based on give strings.
+"""
+
+jewels = "aA"
+stones = "aAAbbbb"
 d = {}
 
-for i in a: 
+for i in stones: 
     if i in d.keys(): 
         d[i] += 1
     else: 
         d[i] = 1
-print(d)
+
+count = 0
+
+for i in d.keys(): 
+    if i in jewels: 
+        count += d[i]
+print(count)
 
