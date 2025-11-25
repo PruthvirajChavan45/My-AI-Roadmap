6a41d39c4e74cc78a3597cd93a4044f54b904132
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-25 09:23:02 +0530

    Example 1

--- Python/Day20 Dictionary (Practice).py
+++ Python/Day20 Dictionary (Practice).py
@@ -1,8 +1,23 @@
 """Dictionary Practice Set"""
 
+#...Hashing....
 
+#.....1) Print Unique elements in list.....
+"""
+    display all distinct elements present in the given list.
+"""
 
+a = [1,1,1,2,2,2,3,3,3,3,3,4,4,4,5,5,6,6,6,6,7,7]
 
+d = {}
+
+for i in a: 
+    if i in d.keys(): 
+        d[i] +=1
+    else: 
+        d[i] = 1
+
+print(d.keys())
 
 
 
