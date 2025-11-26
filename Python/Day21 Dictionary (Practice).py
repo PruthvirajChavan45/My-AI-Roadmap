e3de2c4bc63d3119d1acd780df770145726d0ff4
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-26 20:19:49 +0530

    Example 2

--- Python/Day21 Dictionary (Practice).py
+++ Python/Day21 Dictionary (Practice).py
@@ -7,14 +7,49 @@
     heights in decreasing order
 """
 
-names = ["Mary", "Jhon", "Emma"]
-heights = [180, 165, 170]
+# names = ["Mary", "Jhon", "Emma"]
+# heights = [180, 165, 170]
 
-d = {}
-for i in range(len(names)): 
-    d[heights[i]] = names[i]
-d = sorted(d.items(), key=lambda x:x[0], reverse = True)
+# d = {}
+# for i in range(len(names)): 
+#     d[heights[i]] = names[i]
+# d = sorted(d.items(), key=lambda x:x[0], reverse = True)
 
-for i in range(len(d)):
-    names[i] = d[i][1]
-print(names)
+# for i in range(len(d)):
+#     names[i] = d[i][1]
+# print(names)
+
+
+#.....2) Check if two strings have same frequency map.....
+"""
+    compare character frequencies of two strings and 
+    check if they match.
+"""
+
+s1 = "aabbcc"
+s2 = "baccab"
+
+if len(s1) == len(s2): 
+    d = {}
+
+    for i in s1:
+        if i in d.keys(): 
+            d[i] += 1
+        else: 
+            d[i] = 1
+
+    for i in s2: 
+        if i in d.keys(): 
+            d[i] -= 1
+        else: 
+            print("An extra element was found")
+
+    for i in d: 
+        if d[i] != 0:
+            print("your elements are not same") 
+            break
+    else: 
+        print("your strings are same")
+
+else: 
+    print("Strings are not same")
