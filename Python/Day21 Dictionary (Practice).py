f467efea527172242826659971e275218290d5b7
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-26 20:33:53 +0530

    Example 3

--- Python/Day21 Dictionary (Practice).py
+++ Python/Day21 Dictionary (Practice).py
@@ -26,30 +26,54 @@
     check if they match.
 """
 
-s1 = "aabbcc"
-s2 = "baccab"
+# s1 = "aabbcc"
+# s2 = "baccab"
 
-if len(s1) == len(s2): 
-    d = {}
+# if len(s1) == len(s2): 
+#     d = {}
 
-    for i in s1:
-        if i in d.keys(): 
-            d[i] += 1
-        else: 
-            d[i] = 1
+#     for i in s1:
+#         if i in d.keys(): 
+#             d[i] += 1
+#         else: 
+#             d[i] = 1
 
-    for i in s2: 
-        if i in d.keys(): 
-            d[i] -= 1
-        else: 
-            print("An extra element was found")
+#     for i in s2: 
+#         if i in d.keys(): 
+#             d[i] -= 1
+#         else: 
+#             print("An extra element was found")
 
-    for i in d: 
-        if d[i] != 0:
-            print("your elements are not same") 
-            break
+#     for i in d: 
+#         if d[i] != 0:
+#             print("your elements are not same") 
+#             break
+#     else: 
+#         print("your strings are same")
+
+# else: 
+#     print("Strings are not same")
+
+
+
+#.....3) Find duplicates in array using hashset.....
+"""
+    detect and print elements that appear more than once
+    in the array.
+"""  
+
+a = [1,1,3,3,3,5,5,6,6,6,3,5,3,2,3,4,6,7,8,8,0]
+d = {}
+
+for i in a: 
+    if i in d.keys(): 
+        d[i] += 1
     else: 
-        print("your strings are same")
+        d[i] = 1
 
-else: 
-    print("Strings are not same")
+for i in d.keys():
+    if d[i] > 1: 
+        print(i) 
+    
+
+
