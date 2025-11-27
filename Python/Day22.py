5b06c813233f468bbe710e89243fe3fe9d56847b
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-27 17:58:12 +0530

    map

--- Python/Day22.py
+++ Python/Day22.py
@@ -18,12 +18,38 @@
 #....Examples :- 
 
 # 1) Example: 
-square = lambda x : x ** 2
-print(square(12))       # 144
+# square = lambda x : x ** 2
+# print(square(12))       # 144
 
-# 2) Example: 
-add = lambda a, b : a + b
-print(add(10,10))       # 20
+# # 2) Example: 
+# add = lambda a, b : a + b
+# print(add(10,10))       # 20
 
+
+
+""".....1) Map......"""
+#   - Apply function to every item of an iterable
+#       and return a new iterable.
+
+
+#....Syntax :- map(function , iterable)
+
+#....Examples :- 
+
+# 1) Example : 
+# a = [1,2,3,4,5]
+# l = map(lambda x: x ** 2, a)
+
+# print(list(l))          # [1, 4, 9, 16, 25]
+
+
+# 2) Example : 
+a = [1,2,3,4,5,6,7,8]
+
+def square(x): 
+    return x ** 2
+
+l = map(square, a)
+print(list(l))      # [1, 4, 9, 16, 25, 36, 49, 64]
 
 
