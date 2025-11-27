0039cd6854cc2806d2d9d5cb68418dc78a16bcea
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-27 17:44:46 +0530

    Lambda Expression

---
+++ Python/Day22.py
@@ -0,0 +1,29 @@
+
+"""....Advance Stuff in Python...."""
+
+""".....Lambda Expression......"""
+#      - It is known as lambda expression or lambda function.
+#      - a small, anonymous (nameless) function.
+#      - written in one line.
+#      - used when you need a quick function for 
+#          for a short time
+#      - It is created using (lambda) keyword
+
+
+#....Syntax :- lambda arguments : expression
+
+#       - you can treat like a mini-function.
+
+
+#....Examples :- 
+
+# 1) Example: 
+square = lambda x : x ** 2
+print(square(12))       # 144
+
+# 2) Example: 
+add = lambda a, b : a + b
+print(add(10,10))       # 20
+
+
+
