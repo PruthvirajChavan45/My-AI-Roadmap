4f22e4c9134a04bb1ed818ead0b8529ae01c2f30
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-28 16:17:41 +0530

    Modes Example

--- Python/Day23.py
+++ Python/Day23.py
@@ -80,3 +80,16 @@
 # "w" - overwriting the file. Creates if it does not exist. 
 # "x" - creates a file . error if file already exist. 
 
+
+#......Example.....
+
+open("pull.txt",'x')  # creating a file
+
+file = open("push.txt",'w')  # creating a file and writing the content
+file.write("hello this is a sample file that I have created") # content
+
+file = open("push.txt",'a')  # appeding the content
+file.write("Appended Content")
+
+file.close() #close the file
+
