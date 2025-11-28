3deffdc2d2584e11519627b9b3554dca8d78bdea
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-28 13:44:56 +0530

    Exception Handling

---
+++ Python/Day23.py
@@ -0,0 +1,14 @@
+""".....Exception Handling......"""
+
+#   - Exceptions are raised when the program is syntactically
+#       correct, but the code results in an error. this error does not 
+#       stop the execution of the program, however, it changes the 
+#       normal flow of the program.
+
+#   - So you cannot handle the syntax error. but if there is no syntax error 
+#      you can handle them.
+
+
+
+
+
