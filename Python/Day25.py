87545bbc9d2dc3e31ebf517a941299104d5cf9e0
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-11-30 23:07:09 +0530

    Class in OOPs

---
+++ Python/Day25.py
@@ -0,0 +1,35 @@
+
+""".....OOPs......"""
+#   - OOPs stands for Object Oriented Programming
+
+
+""".....Class......"""
+#   - A class is a blueprint or template to create objects.
+
+#.....Example.....
+
+# Think of a class like:
+#   • A design
+#   • A plan
+#   • A template
+#  - It does not hold real data until an object is created.
+
+
+#.....Example :  
+
+class Factory:
+    a = 12  #attribute 
+    def hello():   #method 
+        print("how are you")
+
+
+print(Factory.a)
+Factory.hello()
+
+
+# Class     : To make a class you can just use the class keyword
+# Attribute : A variable stored inside a class is Attribute.
+# Method    : A function stored inside a class is Method.
+
+# (Note : To call the Attribute or Method you have to first call the class
+#         then you can call specific attribute or method.)
