4c178f7498d1d9df6a48038317453a29adea7e2b
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-01 13:16:10 +0530

    constructor

---
+++ Python/Day26.py
@@ -0,0 +1,9 @@
+
+"""......Constructor......"""
+
+#   - A constructor is a special method in a class that runs
+#     automatically when an object is created.
+#   - Constructor is a special method named __init__.
+#   - Used to initialize object data.
+#   - self refers to the current object.
+#   - self.var creates instance variables.
