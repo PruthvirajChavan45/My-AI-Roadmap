8f7145c8daa7c50e80c9bdc58a0fd61295205fc8
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-02 22:23:45 +0530

    single level inheritance

--- Python/Day27.py
+++ Python/Day27.py
@@ -3,4 +3,21 @@
 #   - Inheritance is the mechanism by which a class (child) can
 #     use the properties and methods of another class (parent).
 
+"""......Types of Inheritance ......"""
+
+"""......1) Single-Level Inheritance......"""
+#   - When one child class inherits from one parent class.
+
+# One parent → One child
+
+# Example....
+class Animal:
+    def sound(self):
+        print("Animal makes sound")
+
+class Dog(Animal):   # child inherits Animal
+    pass
+
+d = Dog()
+d.sound()
 
