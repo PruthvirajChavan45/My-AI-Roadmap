b61d81b1e234fc7d054ca3f6f73719298aa0f623
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-04 15:38:33 +0530

    method overloading

--- Python/Day29.py
+++ Python/Day29.py
@@ -7,3 +7,10 @@
 """......Types of Polymorphism......"""
 #   1) Method Overloading
 #   2) Method Overriding 
+
+"""......1) Method Overloading......"""
+#   - Python simulates method overloading using default or variable - length 
+#     arguments, as it doesn't support traditional overloading.
+#   - same method name, different number of parameters with same class. 
+#   - this happens in languages like java / c++ 
+#   - Python does ot support this style
