2b33e334f51ae32286d3f337ac5ee88da1134f41
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-04 15:28:58 +0530

    Polymorphism

---
+++ Python/Day29.py
@@ -0,0 +1,5 @@
+"""......Polymorphism......"""
+#   - Polymorphism means one thing having many forms.
+#   - Polymorphism allows different classes to define methods with the 
+#     same name but different behaviors.
+#   - In Python, it is typically achieved through method overriding.
