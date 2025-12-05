dba871acea97bdbf7c96dd4db88fa9edef67400f
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-05 12:53:09 +0530

    Introduction of data abstraction

---
+++ Python/Day30.py
@@ -0,0 +1,8 @@
+"""......Abstraction......"""
+#   - Abstraction = Hiding unnecessary details and showing only what is needed.
+#   - It allows the user to focus on what something does, not how it does it.
+
+# Real-life example:
+#   - You drive a car → you use steering, brake, accelerator
+#   - You don't see engine internals
+# This is abstraction.
