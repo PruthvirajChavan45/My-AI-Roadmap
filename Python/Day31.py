f0798d7a9fcdded3959cbd2d943137873d45ab0c
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-06 15:30:59 +0530

    __eq__ method

--- Python/Day31.py
+++ Python/Day31.py
@@ -25,17 +25,40 @@
 
 """..... 2) String Representation :— __str__ ......"""
 
-class Student: 
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+# class Student: 
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
 
-    # Controls how the object prints.
-    def __str__(self): 
-        return f"Your name is {self.name} and your marks are {self.marks}"
+#     # Controls how the object prints.
+#     def __str__(self): 
+#         return f"Your name is {self.name} and your marks are {self.marks}"
     
-obj = Student("Pruthviraj", 98)
-# When you do:
-print(obj)
+# obj = Student("Pruthviraj", 98)
+# # When you do:
+# print(obj)
 
-#   - Python calls __str__()
+# #   - Python calls __str__()
+
+
+"""..... 3) Operator Overloading :— __add__, __eq__, etc. ......"""
+
+# Example: == operator → __eq__
+
+class Equalto: 
+    def __init__(self, number):
+        self.number = number
+
+    def __eq__(self, custom):
+        return self.number == custom.number
+
+obj1 = Equalto(10)
+obj2 = Equalto(10)
+
+print(obj1 == obj2)  # Ture
+
+#.....Python converts:
+# obj1 == obj2
+
+#.....into:
+# obj1.__eq__(obj2)
