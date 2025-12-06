4b65c4b92891735d02bb0ee09220082c959d138c
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-06 15:10:43 +0530

    dunder methods

---
+++ Python/Day31.py
@@ -0,0 +1,10 @@
+""".....Dunder/Magic Methods......"""
+#   - Dunder = Double UNDERSCORE
+#   - These are special methods whose names 
+#     start and end with __ (double underscore).
+
+# Example:
+# __init__, __str__, __len__, __add__, __eq__
+
+# Why “magic”?
+#   - Because Python automatically calls them behind the scenes.
