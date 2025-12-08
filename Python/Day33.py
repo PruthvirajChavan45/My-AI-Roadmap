83663ee5b06467552715c5d8b43eb010c491a65e
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-08 17:26:33 +0530

    Module

---
+++ Python/Day33.py
@@ -0,0 +1,14 @@
+
+"""..... Module ......"""
+#   - A module is just a Python file (.py) that contains
+#     functions, classes, or variables.
+
+#...Example:
+#   - math.py : is a module
+#   - random.py : is a module
+#   - yourcode.py : is also a module
+
+#....Purpose of a Module
+#   - To organize code
+#   - To reuse functions
+#   - To avoid writing long code in one file.
