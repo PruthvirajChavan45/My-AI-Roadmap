4e1d509971c4f1385681c7d8c7f71ccf05b73970
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-08 18:08:02 +0530

    importing the module

--- Python/Day33.py
+++ Python/Day33.py
@@ -12,3 +12,21 @@
 #   - To organize code
 #   - To reuse functions
 #   - To avoid writing long code in one file.
+
+
+"""
+Example: Creating Our Own Module
+
+File: calculator.py
+
+This file contains calculator functions such as add, subtract, multiply, divide.
+This file will behave as a MODULE that can be imported by another Python file.
+
+Path Example: Python/calculator.py
+Usage Example:
+    import calculator
+"""
+
+import calculator      # importing the calculator module
+print(calculator.add(5, 10))   # calling add() function from the module
+
