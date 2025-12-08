a4d79e64b2633c6c8e27258673c89422930864de
Pruthviraj Chavan <pruthvirajchavan6877@gmail.com>
2025-12-08 17:55:02 +0530

    Example of Day 33 module contain different functions

---
+++ Python/calculator.py
@@ -0,0 +1,19 @@
+# Day33 - Modules Example
+# This file contains calculator functions
+# Import this file in another python script to use these functions
+
+# Day33 - Modules Example - Calculator
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b != 0:
+        return a / b
+    return "Cannot divide by zero"
